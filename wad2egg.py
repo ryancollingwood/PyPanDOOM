2d6d8dc6e7b3dfecd247a44733e079aad72fcd2b
Ryan Collingwood <ryancollingwood@gmail.com>
2019-12-25 12:29:21 +1100

    more generic way of writing out csvs

--- wad2egg.py
+++ wad2egg.py
@@ -1,7 +1,7 @@
 import math, argparse, os, sys, re
 from typing import List
 from omg import txdef, wad, mapedit, util
-
+from panda3d.core import *
 
 class Wad2Egg:
     def __init__(
@@ -43,60 +43,89 @@ class Wad2Egg:
         print("Writing %s" % egg_file)
         edit = mapedit.MapEditor(input_wad.maps[level_name.upper()])
 
-        sectors = self.get_sectors(level_name, edit)
+        # first lets get into the proper coordinate system
+        v = edit.vertexes[0]
+        bb_min = mapedit.Vertex(v.x, v.y)
+        bb_max = mapedit.Vertex(v.x, v.y)
+        for v in edit.vertexes:
+            v.x = -v.x
+            if bb_max.x > v.x:
+                bb_max.x = v.x
+            if bb_max.y > v.y:
+                bb_max.y = v.y
+            if bb_min.x < v.x:
+                bb_min.x = v.x
+            if bb_min.y < v.y:
+                bb_min.y = v.y
 
-    def write_csv(self, level_name, collection_name, headers: List, items):
+        sectors = self.get_sectors(level_name, edit)
+        vertexes = self.get_vertexes(level_name, edit)
+        linedefs = self.get_linedefs(level_name, edit, vertexes)
+
+
+    def add_vertex_points(self, sector_index, sectors, vertex_a, vertex_b):
+        result = list()
+        if sector_index > -1:
+            for z in sectors[sector_index]:
+                result.append(Vec3(vertex_a[0], vertex_a[1], z))
+                result.append(Vec3(vertex_b[0], vertex_b[1], z))
+        else:
+            result.append(Vec3(vertex_a[0], vertex_a[1], 0))
+            result.append(Vec3(vertex_b[0], vertex_b[1], 0))
+        return result
+
+    def write_csv(self, level_name, collection_name, items):
+        if len(items) == 0:
+            return
         # do this instead: https://stackoverflow.com/a/109106/2805700
         file_name = f"{self.output_dir}/{level_name}_{collection_name}.csv".lower()
         print(f"csv: {file_name}")
 
+        headers = ["index"] + list(vars(items[0]).keys())
+
         with open(file_name, "w") as out:
-            csv_header = ",".join(["index"]+headers)
+            csv_header = ",".join(headers)
             out.write(f"{csv_header}\n")
             for i, item in enumerate(items):
-                values = [str(i)]
 
-                for attr in headers:
-                    value = item.__getattribute__(attr)
-                    if value is None:
-                        value = ""
-                    values.append(str(value))
-
+                values = [str(i)] + [str(x) if x is not None else "" for x in vars(item).values()]
                 values_csv = ",".join(values)
                 out.write(f"{values_csv}\n")
-                print(values_csv)
+
+        return file_name
 
     def get_sectors(self, level_name, edit):
         self.write_csv(
             level_name,
             "sectors",
-            [
-                "type",
-                "tag",
-                "z_floor",
-                "z_ceil",
-                "tx_floor",
-                "tx_ceil",
-                "light",
-            ],
-            edit.sectors
+            edit.sectors,
         )
 
-    def get_linedefs(self, level_name, edit):
+        result = [Vec2(x.z_floor, x.z_ceil) for x in edit.sectors]
+        return result
+
+    def get_vertexes(self, level_name, edit):
+        self.write_csv(
+            level_name,
+            "vertexes",
+            edit.vertexes,
+        )
+
+        result = [Vec2(v.x, v.y) for v in edit.vertexes]
+        return result
+
+    def get_linedefs(self, level_name, edit, vertexes):
         self.write_csv(
             level_name,
             "linedefs",
-            [
-                "type",
-                "tag",
-                "z_floor",
-                "z_ceil",
-                "tx_floor",
-                "tx_ceil",
-                "light",
-            ],
-            edit.linedefs
+            edit.linedefs,
         )
+
+        return [
+            (vertexes[l.vx_a], vertexes[l.vx_b]) for l in edit.linedefs
+        ]
+
+
 
 
 def parse_args():
